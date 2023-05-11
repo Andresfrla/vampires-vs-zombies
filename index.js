2b681884878bfb643b1ea4b15216a45bf86bc2cb
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-05-11 06:06:12 -0600

    Se pintan los zombies y se pone la funcion check collision para restarle vidas al vampiro

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext('2d');
 let requestId;
 let vampireFrames = 0;
 let gravity = 9.0;
-let blocks = []
+let obstacles = []
 
 function clearCanvas (){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -19,6 +19,33 @@ function vampireAnimation (){
     }
 }
 
+function checkCollisions (){
+    if (vampire.x <= 0 ){
+        vampire.x = canvas.width - vampire.width;
+    } else if (vampire.x > canvas.width) {
+        vampire.x = 0;
+    }
+
+    obstacles.forEach((zombies, i) => {
+        if(vampire.isTouching(zombies)){
+            obstacles.splice(i, 1);
+            vampire.hp--;
+        }
+    })
+}
+
+function generateZombies (){
+    if (vampireFrames % 150 === 0){
+        const randomPosition = Math.floor(Math.random() * canvas.width - 50)
+        const zombies = new Zombie(randomPosition);
+        obstacles.push(zombies)
+    }
+}
+
+function drawZombies(){
+    obstacles.forEach(zombies => zombies.draw())
+}
+
 function updateGame() {
     vampireFrames++;
     clearCanvas();
@@ -28,9 +55,10 @@ function updateGame() {
     vampire.x += vampire.vx; 
     vampire.y += vampire.vy; 
     vampire.y += gravity;
-    //plataform()
-    // git 
     block.draw(); 
+    generateZombies();
+    drawZombies();
+    checkCollisions();
     if (requestId){
         requestAnimationFrame(updateGame);
     }
@@ -124,6 +152,12 @@ class Vampire {
         this.position = 3;
     }
 
+    isTouching(obstacle) {
+        return (
+        this.x < obstacle.x + obstacle.width && this.x + this.width > obstacle.x &&
+        this.y < obstacle.y + obstacle.height && this.y + this.height > obstacle.y
+        )
+    }
 }
 
 class Block {
@@ -131,7 +165,7 @@ class Block {
         this.x = 100;
         this.y = 200;
         this.x1 = 500;
-        this.y1 = 270;
+        this.y1 = 250;
         this.width = 200;
         this.height = 40;
         this.img = new Image();
@@ -147,11 +181,62 @@ class Block {
     }
 }
 
+class Zombie {
+    constructor(x) {
+        this.width = 150;
+        this.height = 150;
+        this.y =  canvas.height;
+        this.x = x;
+        this.animate = 0; // Movimiento de izquierda a derecha
+        this.position = 0;
+        this.img = new Image();
+        this.img.src = '/sources/zombieSprite.png'
+        this.img.onload = () => {
+            this.draw()
+        }
+    }
+
+    draw (){
+        if (this.y > canvas.height - this.height) {
+            this.y = canvas.height - this.height 
+        } else {
+            this.vy++
+        } 
+
+        ctx.drawImage(
+            this.img,
+            (this.animate * 768) / 8,
+            (this.position * 192) / 2,
+            768/8,
+            192/2,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        )
+    }
+
+    moveLefth() {
+        this.position = 1;
+    }
+    
+    moveRigth() {
+        this.position = 0;
+    }
+
+    isTouching(obstacle) {
+        return (
+        this.x < obstacle.x + obstacle.width && this.x + this.width > obstacle.x &&
+        this.y < obstacle.y + obstacle.height && this.y + this.height > obstacle.y
+        )
+    }
+}
+
 // INSTANCES
 const background = new Background();
 const vampire = new Vampire();
 const block = new Block();
-
+const zombies = new Zombie();
 
 // LISTENERS
 
